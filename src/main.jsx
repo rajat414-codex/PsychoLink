fe5ffba52aa5e1a31397efede2a55bd553213e36
Rajat Kamal <rajatkamal2007@gmail.com>
2026-07-01 01:17:14 +0530

    Removed unused google package from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // Ya agar Auth.jsx use kar rahe ho toh us hisaab se
 import './index.css'
-// Official Google Auth Provider
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Aapki exact Google Client ID yahan configure kar di hai */}
-    <GoogleOAuthProvider clientId="996022316929-8g0ov183hp224kg68cps575s9o507jac.apps.googleusercontent.com">
-      <App />
-    </GoogleOAuthProvider>
+    <App />
   </React.StrictMode>,
 )
