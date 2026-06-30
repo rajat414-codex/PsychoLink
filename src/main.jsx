27a31f40eeba7248136154b2abbb153de1570923
Rajat Kamal <rajatkamal2007@gmail.com>
2026-07-01 01:21:44 +0530

    Fix import statement for App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // Ya agar Auth.jsx use kar rahe ho toh us hisaab se
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
